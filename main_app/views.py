b262948848bf14c9d46ed08362d255a1dec9aed1
JMoney-216 <jdm216@gmail.com>
2019-12-19 20:56:00 -0600

    converts links in base.html to url templates adds signup page tested and looks like it signed up  Changes to be committed: 	modified:   main_app/templates/base.html 	new file:   main_app/templates/registration/login.html 	new file:   main_app/templates/registration/signup.html 	modified:   main_app/urls.py 	modified:   main_app/views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 
@@ -12,3 +13,19 @@ def home(request):
 
 def about(request):
   return render(request, 'about.html')
+
+def signup(request):
+  error_message = ''
+  if request.method == 'POST':
+    # This is how to create a 'user'form object
+    # that include the data form browser
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+      user = form.save()
+      login(request, user)
+      return redirect('home')
+    else:
+      error_message = 'Invalid sign up - please try again'
+  form = UserCreationForm()
+  context = {'form': form, 'error_message': error_message}
+  return render(request, 'registration/signup.html', context)
