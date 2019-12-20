b2ab2f1a5c3ecc45500e105d6c8c551a7af57bd3
JMoney-216 <jdm216@gmail.com>
2019-12-19 20:45:22 -0600

    adds 'about' view and updates navbar link tested and working
     Changes to be committed:
    	new file:   main_app/templates/about.html
    	modified:   main_app/templates/base.html
    	modified:   main_app/urls.py
    	modified:   main_app/views.py

--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+
+from .models import Profile, Company
 
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
