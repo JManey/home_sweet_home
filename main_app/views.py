7d2a8b344f637c48a108aa411fe9345b4e1a1c72
drmartirosian <55171534+drmartirosian@users.noreply.github.com>
2019-12-23 13:10:20 -0600

    ???

--- main_app/views.py
+++ main_app/views.py
@@ -1,47 +1,23 @@
-<<<<<<< HEAD
-from django.shortcuts import render
-=======
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.views.generic import ListView, DetailView
->>>>>>> master
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-<<<<<<< HEAD
-=======
 from .models import Profile, Company, Property
 
->>>>>>> master
 
 # Create your views here.
 def home(request):
   return render(request, 'home.html')
 
-<<<<<<< HEAD
-=======
 def about(request):
   return render(request, 'about.html')
->>>>>>> master
 
 def signup(request):
   error_message = ''
   if request.method == 'POST':
-<<<<<<< HEAD
-    # This is how to create a 'user' form object
-    # that includes the data from the browser
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-      # This will add the user to the database
-      user = form.save()
-      # This is how we log a user in via code
-      login(request, user)
-      return redirect('index')
-    else:
-      error_message = 'Invalid sign up - try again'
-  # A bad POST or a GET request, so render signup.html with an empty form
-=======
     # This is how to create a 'user'form object
     # that include the data form browser
     form = UserCreationForm(request.POST)
@@ -51,17 +27,10 @@ def signup(request):
       return redirect('home')
     else:
       error_message = 'Invalid sign up - please try again'
->>>>>>> master
   form = UserCreationForm()
   context = {'form': form, 'error_message': error_message}
   return render(request, 'registration/signup.html', context)
 
-<<<<<<< HEAD
-
-# @login_required
-
-# class ClassName(   LoginRequiredMixin,       OtherStuff, OtherStuff):
-=======
 def properties_index(request):
   properties = Property.objects.all()
   return render(request, 'properties/index.html', {'properties': properties})
@@ -86,4 +55,3 @@ class PropertyCreate(LoginRequiredMixin, CreateView):
 class PropertyUpdate(LoginRequiredMixin, UpdateView):
   model = Property
   fields = '__all__'
->>>>>>> master
